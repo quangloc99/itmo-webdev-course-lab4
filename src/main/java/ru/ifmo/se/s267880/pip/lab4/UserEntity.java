c188489a3f75d81bad3abb1db162156dde340855
Tran Quang Loc <quangloctran99@gmail.com>
2019-12-12 00:14:05 +0300

    Add UserEntity.

---
+++ src/main/java/ru/ifmo/se/s267880/pip/lab4/UserEntity.java
@@ -0,0 +1,45 @@
+package ru.ifmo.se.s267880.pip.lab4;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String passwordHash;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String email, String passwordHash) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
