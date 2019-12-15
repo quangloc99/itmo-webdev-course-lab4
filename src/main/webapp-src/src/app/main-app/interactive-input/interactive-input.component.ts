28a43a69ad0b91ac64455e8fe679bc64ff49d07c
Tran Quang Loc <quangloctran99@gmail.com>
2019-12-16 00:50:00 +0300

    Add slider for parameter R in interactive input.

--- src/main/webapp-src/src/app/main-app/interactive-input/interactive-input.component.ts
+++ src/main/webapp-src/src/app/main-app/interactive-input/interactive-input.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {Point} from "../../../helpers/utils";
+import {Component, Input, OnInit} from '@angular/core';
+import {FieldRanges, Point} from "../../../helpers/utils";
 
 @Component({
   selector: 'app-interactive-input',
@@ -7,6 +7,8 @@ import {Point} from "../../../helpers/utils";
   styleUrls: ['./interactive-input.component.css']
 })
 export class InteractiveInputComponent implements OnInit {
+  @Input() inputFieldRanges: FieldRanges;
+  parameterR: number = 3;
 
   constructor() { }
 
