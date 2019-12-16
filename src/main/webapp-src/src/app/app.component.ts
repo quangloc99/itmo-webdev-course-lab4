5ff4ffe4912cf4588a2007ee46c8c3f7ee7b3d4a
Tran Quang Loc <quangloctran99@gmail.com>
2019-12-16 16:15:44 +0300

    Align element for input-form component.

--- src/main/webapp-src/src/app/app.component.ts
+++ src/main/webapp-src/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {screenSizeListener} from "../helpers/screen-size-listener";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webapp-src';
+
+  ngOnInit(): void {
+    screenSizeListener([
+      {size: 764, name: 'mobile'},
+      {size: 1138, name: 'tablet'},
+      {size: Infinity, name: 'desktop'},
+    ], (environment, oldEnvironment) => {
+      console.log(`change screen size from ${oldEnvironment} to ${environment}`);
+      if (oldEnvironment) document.body.classList.remove(oldEnvironment);
+      document.body.classList.add(environment);
+    });
+  }
 }
