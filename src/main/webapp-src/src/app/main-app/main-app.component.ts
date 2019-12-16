559145d2d43c60faa0155f2776556756b8a3b0ed
Tran Quang Loc <quangloctran99@gmail.com>
2019-12-16 18:40:08 +0300

    Align elements in result-table component.

--- src/main/webapp-src/src/app/main-app/main-app.component.ts
+++ src/main/webapp-src/src/app/main-app/main-app.component.ts
@@ -15,6 +15,18 @@ export class MainAppComponent implements OnInit {
     {id: 2, x: -1, y: 2, r: 4, result: false},
     {id: 3, x: 3, y: -2, r: 2, result: false},
     {id: 4, x: -5, y: -3, r: 5, result: true},
+    {id: 1, x: 1, y: 2, r: 3, result: true},
+    {id: 2, x: -1, y: 2, r: 4, result: false},
+    {id: 3, x: 3, y: -2, r: 2, result: false},
+    {id: 4, x: -5, y: -3, r: 5, result: true},
+    {id: 1, x: 1, y: 2, r: 3, result: true},
+    {id: 2, x: -1, y: 2, r: 4, result: false},
+    {id: 3, x: 3, y: -2, r: 2, result: false},
+    {id: 4, x: -5, y: -3, r: 5, result: true},
+    {id: 1, x: 1, y: 2, r: 3, result: true},
+    {id: 2, x: -1, y: 2, r: 4, result: false},
+    {id: 3, x: 3, y: -2, r: 2, result: false},
+    {id: 4, x: -5, y: -3, r: 5, result: true},
   ];
 
   constructor(private http: HttpClient) { }
