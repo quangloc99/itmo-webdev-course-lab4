888afb83dc71373046c21204afe93e385ded1c70
Tran Quang Loc <quangloctran99@gmail.com>
2019-12-27 00:41:28 +0300

    Add validation for user-entry component.

--- src/main/webapp-src/src/app/user-entry/user-entry.component.ts
+++ src/main/webapp-src/src/app/user-entry/user-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-user-entry',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-entry.component.css']
 })
 export class UserEntryComponent implements OnInit {
+  submitting = false;
+  emailControl = new FormControl("", [
+    Validators.required,
+    Validators.email
+  ]);
+
+  passwordControl = new FormControl("", [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+
+  confirmPasswordControl = new FormControl("", [
+    Validators.required,
+    (control) => {
+      if (control.value !== this.passwordControl.value) {
+        return {"unmatch-confirm-password": {}};
+      }
+      return null;
+    }
+  ]);
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  hasLoginValidationError() {
+    return !!(this.emailControl.errors || this.passwordControl.errors);
+  }
+
+  hasRegisterValidationError() {
+    return !!(this.emailControl.errors || this.passwordControl.errors || this.confirmPasswordControl.errors);
+  }
 }
